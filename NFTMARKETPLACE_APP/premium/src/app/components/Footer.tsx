92006dde9daaca61abc263f2fa5ecf3a0f58221c
ni9eball <peterkayode2018@gmail.com>
2025-02-12 10:45:33 +0100

    added github link

--- NFTMARKETPLACE_APP/premium/src/app/components/Footer.tsx
+++ NFTMARKETPLACE_APP/premium/src/app/components/Footer.tsx
@@ -6,19 +6,28 @@ import { FaGithub } from 'react-icons/fa6';
 
 const Footer = () => {
   return (
-      <div className="bg-black text-gray-400 w-full min-h-[480px] flex flex-col justify-end items-center relative  p-20 md:flex-row md:justify-around md:items-end">
+    <div className="bg-black text-gray-400 w-full min-h-[480px] flex flex-col justify-end items-center relative  p-20 md:flex-row md:justify-around md:items-end">
       <div className="flex flex-col items-start w-full md:w-1/3 space-y-4">
         <h3 className="text-2xl font-medium">Work of art</h3>
-        <p className="text-base">Made with <span className="text-[#BA6573]">❤</span> by Iam9ball </p>
+        <p className="text-base">
+          Made with <span className="text-[#BA6573]">❤</span> by Iam9ball{" "}
+        </p>
         <div className="flex space-x-4">
           <div className="w-8 h-8 bg-gray-800 rounded-md flex justify-center items-center">
-           <FaLinkedin/>
+            <FaLinkedin />
           </div>
           <div className="w-8 h-8 bg-gray-800 rounded-md flex justify-center items-center">
-           <FaSquareXTwitter/>
+            <FaSquareXTwitter />
           </div>
           <div className="w-8 h-8 bg-gray-800 rounded-md flex justify-center items-center">
-            <FaGithub/>
+            <a
+              href={`https://github.com/iam9ball/premium-nftMarketplace`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-800 hover:underline"
+            >
+              <FaGithub />
+            </a>
           </div>
         </div>
         <p className="text-gray-500 text-sm">2024 © All Rights Reserved</p>
@@ -35,7 +44,7 @@ const Footer = () => {
         <p className="text-base">Join our team</p>
         <p className="text-base">Partner with us</p>
       </div>
-      </div>
+    </div>
   );
 };
 
